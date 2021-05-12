e1e1ec843c78e08bc8d8070257e01e5c419c4579
pgf <930889352@qq.com>
2021-05-12 13:56:53 +0800

    新添加了一个newMain方法

--- src/com/gec/test/TestDemo.java
+++ src/com/gec/test/TestDemo.java
@@ -3,11 +3,11 @@ package com.gec.test;
 public class TestDemo {
 
 
-    public void main(){
+    public void main() {
         System.out.println("这是编写的第一个方法");
     }
 
-    public void test01(){
+    public void test01() {
         System.out.println("这是一个测试方法");
         System.out.println("---------");
 
@@ -16,6 +16,9 @@ public class TestDemo {
     }
 
 
-
+    //新添加的方法
+    public void newMain() {
+        System.out.println("这是新添加的一个方法");
+    }
 
 }
