9d0e1054d83f5dc87f89dcf0d41e46b5bd0d1300
pgf <930889352@qq.com>
2021-05-12 11:04:35 +0800

    第一次提交

---
+++ src/com/gec/test/TestDemo.java
@@ -0,0 +1,19 @@
+package com.gec.test;
+
+public class TestDemo {
+
+    private int id;
+
+    public void main(){
+        System.out.println("这是编写的第一个方法");
+    }
+
+    public void test01(){
+        System.out.println("这是一个测试方法");
+        System.out.println("---------");
+    }
+
+
+
+
+}
